06826d0ffcc105fc2466630d5f5c96718d4ad4a6
Gavin Baker <gavnbaker@live.com>
2018-07-22 10:24:15 -0400

    Updated movie model with required fields Created movie migration

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +9,24 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "Date Added")]
         public DateTime DateAdded { get; set; }
+
+        [Required]
+        [Display(Name = "Number in Stock")]
         public int Stock { get; set; }
 
         public Genre Genre { get; set; }
+
+        [Required]
         public byte GenreId { get; set; }
         
     }
